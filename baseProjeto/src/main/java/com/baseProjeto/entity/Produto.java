c88694371aab69ecda605d90838b5ad187cbd6f7
EduardoJunio <eduardojvr@gmail.com>
2020-10-12 17:07:00 -0300

    Üpdate project

---
+++ baseProjeto/src/main/java/com/baseProjeto/entity/Produto.java
@@ -0,0 +1,43 @@
+package com.baseProjeto.entity;
+
+public class Produto {
+	private int id;
+	private String codigo;
+	private String urlImg;
+	private String descricao;
+	private String preco;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+	public String getUrlImg() {
+		return urlImg;
+	}
+	public void setUrlImg(String urlImg) {
+		this.urlImg = urlImg;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public String getPreco() {
+		return preco;
+	}
+	public void setPreco(String preco) {
+		this.preco = preco;
+	}
+	
+	
+
+}
